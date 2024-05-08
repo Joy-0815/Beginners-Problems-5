f3b45751ded0d356975a2e45ae19c7e220d17d11
Jocelyn <160691921+Joy-0815@users.noreply.github.com>
2024-05-08 16:26:47 +1200

    Create foodlist.py

---
+++ foodlist.py
@@ -0,0 +1,15 @@
+count = 0
+
+FoodList = []
+food = True
+
+for num in range (0,8)
+    food = input("What is your favourite food?").lower()
+    if food == "pesto":
+      count += 1
+    else:
+      FoodList.append(food)
+        
+print(count,"people like pesto")
+for num in range (count):
+    print("I like pesto")
