984810af7bb9c6b50b9deb3bf4ea70956e297c39
Jocelyn <160691921+Joy-0815@users.noreply.github.com>
2024-05-21 18:59:01 +1200

    Update sse.py

--- sse.py
+++ sse.py
@@ -2,9 +2,9 @@ stringsList = ["abc","123","2332","aBBA","heelloo","1212","DcEfD"]
 
 def first_last(strings):
     lista = []
-    for num in strings:
-        if num[0] == num[-1]:
-            lista.append(num)
+    for word in strings:
+        if len(word) >= 1 and word[0] == word[-1]:
+            lista.append(word)
     return lista
 
 print(first_last(stringsList))
