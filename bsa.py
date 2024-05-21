6ceadbd1cfd5c6aeea714c2048950c7b8d06450c
Jocelyn <160691921+Joy-0815@users.noreply.github.com>
2024-05-21 18:52:11 +1200

    Update bsa.py

--- bsa.py
+++ bsa.py
@@ -1,10 +1,11 @@
+numsList = [7, 6, 23, 8.18, 18, 8, 7.2, 85, 915, 12]
 numsList.sort()
 print("Largest number is:", numsList[-1])
-print("Smallest number is:", numsList[1])
+print("Smallest number is:", numsList[0])
 numsList.remove(numsList[-1])
-numsList.remove(numsList[1])
+numsList.remove(numsList[0])
 
 dived = len(numsList)
 
-average = sum(numsList) % dived
+average = sum(numsList) / dived
 print("The average of the rest of the numbers is",average)
